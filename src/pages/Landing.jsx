a5bc217f403c4e55fd9a400347fbb5bd46e4cc09
Wilson <wilson.santiago.car@gmail.com>
2024-05-11 17:59:41 -0500

    Añadidas dependecias de Tailwind y React Router, creados componentes básicos de páginas principales

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,5 @@
+export default function Landing(){
+    return (
+        <h1>Landing page goes here !</h1>
+    )
+}
---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,5 @@
+export default function LogIn(){
+    return (
+        <h1>Log In page goes here !</h1>
+    )
+}
