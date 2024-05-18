c68b8c40ff0329e2e0ae095adc71f60e30412ffa
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2024-05-18 11:49:54 -0500

    Correcion Formulario
    se centro el formulario entre los navbar y fotter

--- src/pages/Pawn_request.jsx
+++ src/pages/Pawn_request.jsx
@@ -35,7 +35,7 @@ export default function Pawn_request() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-6 mt-8 border border-gray-300 rounded-lg shadow-md">
+    <div className="max-w-2xl mx-auto p-6 mt-8 mb-8 border border-gray-300 rounded-lg shadow-md">
       <h1 className="text-2xl font-bold text-center mb-6 font-poppins text-azulOscuro">Solicitud de Empeño</h1>
       {isSubmitted ? (
         <div className="text-center mb-4">
