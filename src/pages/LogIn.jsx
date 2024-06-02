c680a2f7b190772e39991d8cac25347e02721920
jorge <iefaz.jorgebedoya@gmail.com>
2024-06-01 22:11:31 -0500

    añadi el LogIn y el SingUp

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,5 +1,66 @@
-export default function LogIn(){
-    return (
-        <h1>Log In page goes here !</h1>
-    )
+import React from 'react';
+import hombreEnCarrito from '../assets/hombre_en_carrito.png'; // Asegúrate de que la ruta es correcta
+import logo from '../assets/pawns_blanca.png'; // Importa el logo de tu carpeta assets
+import { Link } from 'react-router-dom';
+function LoginForm() {
+  return (
+    <div className="flex min-h-screen bg-white"> {/* Fondo general blanco */}
+      {/* Div for the image and logo */}
+      <div className="w-1/2 relative" style={{ backgroundColor: "#AD8786" }}>
+        <img src={logo} alt="Logo" className="absolute top-2 left-2" style={{ width: '180px', height: '180px' }} />
+        <div className="h-full bg-no-repeat bg-center" style={{ backgroundImage: `url(${hombreEnCarrito})`, backgroundSize: "65% auto" }}>
+          {/* Empty if only displaying the image */}
+        </div>
+      </div>
+
+      {/* Div for the login form */}
+      <div className="w-1/2 flex items-center justify-center">
+        <div className="bg-white px-16 py-10 w-full max-w-2xl"> {/* Increased max-width of the form container */}
+          <div className="text-3xl font-bold mb-6 text-left">Bienvenido/a</div>
+          <hr className="border-gray-300 my-11" />
+          <form>
+            <div className="mb-5 text-left">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 block w-full px-4 py-5 text-sm border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm rounded-sm"
+                placeholder="xxx@gmail.com"
+              />
+            </div>
+            <div className="mb-6 text-left">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 block w-full px-4 py-5 text-sm border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm rounded-sm"
+                placeholder="************"
+              />
+            </div>
+            <div className="mb-8">
+              <button
+                className="text-white font-bold py-5 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                type="submit"
+                style={{ backgroundColor: "#AD8786" }}
+              >
+                Ingresar
+              </button>
+            </div>
+            <div className="text-center">
+            <span className="text-sm text-gray-700">¿No estás registrado? </span>
+            <Link to="/signup" className="inline-block align-baseline font-medium text-sm text-yellow-600 ">
+                Regístrate ahora
+            </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default LoginForm;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,98 @@
-export default function SignUp(){
-    return (
-        <h1>Sign Up page goes here !</h1>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import mujerConTableta from '../assets/mujer_en_pagina.png';  // Asegúrate de que la ruta es correcta
+import logo from '../assets/pawns_blanca.png'; // Asegúrate de que la ruta del logo es correcta
+
+function SignUp() {
+  return (
+    <div className="flex min-h-screen bg-white">
+      {/* Div for the image with logo */}
+      <div className="w-1/2 relative" style={{ backgroundColor: "#5E1414" }}> {/* Updated background color */}
+        <img src={logo} alt="Logo" className="absolute top-2 left-2" style={{ width: '180px', height: '180px' }} />
+        <div className="h-full bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url(${mujerConTableta})`, backgroundSize: "65% auto" }}>
+          {/* Empty if only displaying the image */}
+        </div>
+      </div>
+
+      {/* Div for the registration form */}
+      <div className="w-1/2 flex items-center justify-center relative">
+        <Link to="/login" className="absolute top-9 left-9 text-gray-500 hover:text-gray-700" style={{ fontSize: '20px' }}> {/* Adjust top and left values */}
+          &larr; Regresar
+        </Link>
+        <div className="bg-white p-12 w-full max-w-lg">
+          <h1 className="text-2xl font-bold mb-2 text-left">Regístrate ahora</h1>
+          <p className="mb-4 text-sm text-gray-400 text-left">¡Regístrate y empieza a negociar!</p> {/* Additional text */}
+          <hr className="border-gray-300 my-8" />
+          <form>
+            <div className="mb-4 text-left">
+              <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
+                Nombre completo:
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500"
+                placeholder="Paulo Coelho"
+              />
+            </div>
+            <div className="mb-4 text-left">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500"
+                placeholder="xxx@email.com"
+              />
+            </div>
+            <div className="mb-4 text-left">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500"
+                placeholder="************"
+              />
+            </div>
+            <div className="mb-4 text-left">
+              <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
+                Género:
+              </label>
+              <select
+                id="gender"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500"
+              >
+                <option>Masculino</option>
+                <option>Femenino</option>
+                <option>Otro</option>
+              </select>
+            </div>
+            <div className="mb-6 text-left">
+              <label htmlFor="birthdate" className="block text-sm font-medium text-gray-700">
+                Fecha de nacimiento:
+              </label>
+              <input
+                type="date"
+                id="birthdate"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500"
+                defaultValue="2024-12-12"
+              />
+            </div>
+            <button
+              className="w-full py-3 px-4 bg-5E1414 text-white rounded-md hover:bg-red-700 focus:outline-none"
+              type="submit"
+              style={{ backgroundColor: "#5E1414" }}  // Updated button background color
+            >
+              Continua
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default SignUp;
