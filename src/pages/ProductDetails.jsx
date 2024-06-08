775cdbd35474f12663fa03e54f2186c1bdf96a84
Wilson <wilson.santiago.car@gmail.com>
2024-06-08 11:10:02 -0500

    pulido detalles de producto

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,24 +5,45 @@ import './ProductDetails.css';
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState(0);
 
   useEffect(() => {
     const products = getProducts();
     const product = products.find((_, index) => index === parseInt(id, 10));
-    setProduct(product);
+    setProduct({
+      img: "https://media.revistagq.com/photos/61bb41b5d398f278a07e2bd8/3:2/w_1335,h_890,c_limit/Longines-VHP-GMT-2018.jpeg",
+      category: "Moda",
+      name: "Reloj",
+      price: 200,
+      description: 'Lorem ipsum dolor sit amet'
+      });
   }, [id]);
 
   if (!product) return <p>Producto no encontrado.</p>;
 
   return (
-    <div className="product-details-container">
-      <div className="product-details">
-        <h2 className="product-title">{product.name}</h2>
-        <img className="product-image" src={product.img} alt={product.name} />
-        <div className="product-info">
-          <p><strong>Marca:</strong> {product.brand}</p>
-          <p><strong>Precio:</strong> {product.price}</p>
+    <div className="flex flex-col items-center py-8">
+      <h2 className=' font-medium text-2xl text-gray-500'>Detalles de producto</h2>
+      <div className="flex gap-x-16 w-full pl-60 pr-48 my-10">
+        <div className="w-2/5">
+          <img className=" w-full h-auto" src={product.img} alt={product.name} />
+        </div>
+        <div className="flex flex-col pl-5 pr-20 pt-5 text-left flex-1">
+          <div className="border-b border-b-gray-200 pb-2 mb-2">
+            <h2 className=" font-semibold text-2xl">{product.name}</h2>
+            <p className='mt-1 text-base font-light text-gray-500'>Categoría: {product.category}</p>
+          </div>
+
+          <p className='mt-8 pb-8 font-medium text-3xl border-b border-b-gray-200'>$ {product.price}</p>
+
+          <button className='mt-7 rounded-full bg-firstColor text-white hover:opacity-80 py-1 px-6 self-center'>
+            Agregar al carrito
+          </button>
+
+          <div className='mt-8 border border-gray-200 p-3 rounded'>
+            <h4 className='pb-2 font-semibold text-lg w-full border-b border-b-gray-200'>Descripción</h4>
+            <p className='mt-4 text-gray-700'>{product.description}</p>
+          </div>
         </div>
       </div>
     </div>
